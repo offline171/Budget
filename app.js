5925efd1fd5f75ba3a5c302ac2c9900ddbfa2647
offline171 <offline.stein@gmail.com>
2025-08-12 21:58:24 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,57 @@
+/////// app.js
+const path = require("node:path");
+const { Pool } = require("pg");
+const express = require("express");
+const session = require("cookie-session");
+const bcrypt = require("bcryptjs");
+const passport = require("passport");
+const LocalStrategy = require('passport-local').Strategy;
+
+const pool = require("./db/pool");
+
+const app = express();
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+
+// 3 functions below are important to create and maintain sessions
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const { rows } = await pool.query("SELECT * FROM users WHERE username = $1", [username]);
+      const user = rows[0];
+
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        // passwords do not match!
+        return done(null, false, { message: "Incorrect password" })
+      }
+      return done(null, user);
+    } catch(err) {
+      return done(err);
+    }
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.user_id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const { rows } = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
+    const user = rows[0];
+
+    done(null, user);
+  } catch(err) {
+    done(err);
+  }
+});
+
+app.listen(3000, () => console.log("app listening on port 3000!"));
---
+++ db/pool.js
@@ -0,0 +1,9 @@
+const { Pool } = require("pg");
+require('dotenv').config();
+const POSTGRES_USER = process.env.POSTGRES_USER;
+const POSTGRES_PASSWORD = process.env.POSTGRES_PASSWORD;
+const POSTGRES_DB = process.env.POSTGRES_DB;
+
+module.exports = new Pool({
+  connectionString: "postgresql://" + POSTGRES_USER + ":" + POSTGRES_PASSWORD + "@db:5432/" + POSTGRES_DB
+});
