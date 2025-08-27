a12cf80f7a7e21b6515d1fa0a4cc7fdcad443852
offline171 <offline.stein@gmail.com>
2025-08-27 19:12:16 -0400

    Added extra datapoint for credit (different from money)

--- routes/signUpRouter.js
+++ routes/signUpRouter.js
@@ -10,7 +10,7 @@ signUpRouter.get("/", (req, res) => {
 signUpRouter.post("/", async (req, res, next) => {
  try {
   const hashedPassword = await bcrypt.hash(req.body.password, 10);
-  await pool.query("insert into users (username, password) values ($1, $2)", [req.body.username, hashedPassword]);
+  await pool.query("insert into users (username, password, money, credit) values ($1, $2, $3, $4)", [req.body.username, hashedPassword, 0, 0]);
   res.redirect("/");
  } catch (error) {
     console.error(error);
